169a4283ecaf9286cd458c73c0ef1dee645aacc1
sensational.mack <mackn478@gmail.com>
2022-11-02 22:57:35 -0400

    began working on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,22 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}], //included associated product 
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+      
+
+
+  // be sure to include its associated Products
+ 
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
